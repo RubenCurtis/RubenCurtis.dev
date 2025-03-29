3b52fec035d43475ad7ed53ae6dcfee841f7ca69
Ruben Curtis <rubencurtis@hotmail.co.uk>
2025-03-29 16:43:31 +0000

    Prototyping animations on projects page.
    TODO get the formatting for individual projects completed (i.e. section, div, name, technologies used, times, links etc.)

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -5,16 +5,17 @@ import Link from "next/link";
 
 export default function Projects() {
   useEffect(() => {
-    // Cool stuff here
+    const header = document.getElementById("header");
+    if (header) setTimeout(() => header.classList.add("fade-in-active"), 0);
   }, []);
   return (
     <section className="relative flex flex-col py-4 px-4 w-full max-w-screen-lg">
-      <header id="header" className="w-full flex justify-between items-center  bg-primary">
+      <header id="header" className="w-full flex justify-between items-center fade-in bg-primary">
         <h1 className="text-white text-xl font-bold"><Link href="/">rubencurtis.dev</Link></h1>
       </header>
       <div className="relative flex flex-col items-start w-full bg-primary text-white p-4">
-        <h1 id="hero-title" className="text-white text-4xl font-bold mb-4">Projects</h1>
-        <p>Still working on it!</p>
+        <h1 id="hero-title" className="text-white text-4xl font-bold mb-4 animate-slide-in-from-top">Projects</h1>
+        <h2 id="projectnamething" className="text-white text-4xl font-bold mb-4 animate-slide-in-from-top">Still working on it!</h2>
       </div>
     <section className="relative flex flex-col py-16 px-4 w-full h-full max-w-screen-lg"></section>
     </section>
